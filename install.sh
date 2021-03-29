054c44f3f806a749226ced674a6bd69cd1588f00
alanwildan <76613101+alanwildan@users.noreply.github.com>
2021-03-29 11:55:48 +0700

    Y

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ apt-get install -y nodejs libwebp ffmpeg wget tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
-echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+echo "[*] Semua tools telah di install, ketik \"npm start\" untuk menjalankan"
