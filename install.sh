39f5ac8f0e594af9df0aea5b1e4030d3aadce7ac
alanwildan <76613101+alanwildan@users.noreply.github.com>
2021-04-07 15:49:26 +0700

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-apt-get update
-apt-get upgrade
-apt-get install -y nodejs libwebp ffmpeg wget tesseract
-wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-npm install
-
-echo "[*] Semua tools telah di install, ketik \"npm start\" untuk menjalankan"
